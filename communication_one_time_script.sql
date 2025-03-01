9b8155c2221fe0113ffeb5341c47211c6322643a
KapilP0123 <er.kapilpachori@gmail.com>
2025-03-02 02:25:32 +0530

    lookup and comm one time script added

---
+++ communication_one_time_script.sql
@@ -0,0 +1,7 @@
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`, `desc`) VALUES ('1', 'SMTP_HOST', 'email-smtp.us-east-1.amazonaws.com', '');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`, `desc`) VALUES ('2', 'SMTP_PORT', '587', '');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`, `desc`) VALUES ('3', 'SMTP_USER', 'Alpide ERP Suppport', '');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`, `desc`) VALUES ('4', 'SMTP_PASSWORD', 'BNW6kZVzX14SSbC8XKp7RkUZ2tSAMw2y/k6eWT5+lsQp', '');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`, `desc`) VALUES ('5', 'SMTP_USERNAME', 'AKIA2JAHJADXBYE74O4D', '');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`) VALUES ('7', 'EMAIL_FROM', 'support@alpide.com');
+INSERT INTO `alpide-communication`.`bn_email_setting` (`email_setting_id`, `key`, `value`) VALUES ('8', 'FROM_NAME', 'Alpide ERP');
