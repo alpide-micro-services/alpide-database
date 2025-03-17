d29f366464c3b9767b4975a93fec2487fd14d64e
Chirag Tyagi <chiragtyagi@Chirags-MacBook-Pro.local>
2025-03-17 17:29:49 +0530

    query

--- release/Ver-1/01-commerce.sql
+++ release/Ver-1/01-commerce.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `alpide-commerce`.`ecom_setting` ADD COLUMN website_content text;
+
+ALTER TABLE `alpide-commerce`.`ecom_setting` ADD COLUMN website_sequence_data text;
+
+ALTER TABLE `alpide-commerce`.`ecom_usp_meta` ADD COLUMN type varchar(200) default null;
