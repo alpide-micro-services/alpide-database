e16c3026dcb95a5caf8d0780d51d5c0628614b99
Chirag Tyagi <chiragtyagi@Chirags-MacBook-Pro.local>
2025-03-20 18:34:46 +0530

    sql add in school

--- release/Ver-1/01-commerce.sql
+++ release/Ver-1/01-commerce.sql
@@ -2,3 +2,7 @@ ALTER TABLE `alpide-commerce`.`ecom_setting` ADD COLUMN website_content text;
 
 ALTER TABLE `alpide-commerce`.`ecom_setting`
 CHANGE COLUMN `website_sequence_data` `website_sequence_data` MEDIUMTEXT NULL DEFAULT NULL ;
+
+
+ALTER TABLE `alpide-education`.`org_registration_form_setting_permissions` ADD COLUMN first_name varchar(140) default null;
+ALTER TABLE `alpide-education`.`org_registration_form_setting_permissions` ADD COLUMN last_name varchar(140) default null;
