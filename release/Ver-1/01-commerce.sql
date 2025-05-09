113c2c097e12039770cd5c607f330b8ebf4d2aa7
Chirag Tyagi <chiragtyagi@Chirags-MacBook-Pro.local>
2025-05-09 19:41:30 +0530

    code sql update till date 9-may-2025

--- release/Ver-1/01-commerce.sql
+++ release/Ver-1/01-commerce.sql
@@ -278,3 +278,163 @@ CREATE TABLE `alpide-crm`.crm_lead_form_response (
 ALTER TABLE `alpide-commerce`.`ecom_setting` ADD COLUMN cart_prefrence text DEFAULT NULL;
  ALTER TABLE `alpide-crm`.`crm_lead_form_response` MODIFY COLUMN extra_details TEXT default null;
 ALTER TABLE `alpide-crm`.`crm_lead_detail` Add COLUMN question_text TEXT default null;
+
+ALTER TABLE `alpide-purchase`.`supplier_debit_memo_applied` MODIFY COLUMN invoice_master_id Bigint default null;
+
+
+DROP PROCEDURE IF EXISTS `alpide-purchase`.get_purchase_invoice_summary;
+DELIMITER $$
+$$
+CREATE  PROCEDURE `alpide-purchase`.`get_purchase_invoice_summary`(IN rid int, in supplierId int, in status varchar(75), in projectMasterId int, in startDate timestamp, in endDate timestamp, in invoiceNumber varchar(45), in userStatus varchar(45), in reference varchar(45), in amount double, in amountSymbol varchar(45), in ledgerAccountId int, IN pageNumber int, IN pageSize int)
+BEGIN
+		declare whereClause TEXT;
+		
+		set whereClause = concat("pi.rid=", rid);
+	
+		IF( supplierId > 0) THEN
+			set whereClause = concat(whereClause, " and pi.supplier_id=", supplierId);
+		END IF;
+		
+        IF(status is not null) THEN
+			set whereClause = concat(whereClause, " and pi.status='",status,"'");
+		END IF;
+        IF(projectMasterId >0) THEN
+			set whereClause = concat(whereClause, " and pi.project_master_id='",projectMasterId,"'");
+		END IF;
+        IF(startDate is not null) THEN
+			set whereClause = concat(whereClause, " and pi.invoice_date between '", startDate, "' and '", endDate, "'");
+		END IF;
+        IF(invoiceNumber is not null) THEN
+			set whereClause = concat(whereClause, " and pi.invoice_number='",invoiceNumber,"'");
+		END IF;
+        IF(userStatus is not null) THEN
+			set whereClause = concat(whereClause, " and pi.status=",user_status);
+		END IF;
+        IF(reference is not null) THEN
+			set whereClause = concat(whereClause, " and pi.reference_number='",reference,"'");
+		END IF;
+        	IF(amount > 0) THEN
+				IF(amountSymbol is not null && amountSymbol = '=') THEN
+				set whereClause = concat(whereClause, " and pi.total_amount = ", amount);
+				END IF;
+				IF(amountSymbol is not null && amountSymbol = '>') THEN
+				set whereClause = concat(whereClause, " and pi.total_amount > ", amount);
+				END IF;
+				IF(amountSymbol is not null && amountSymbol = '<') THEN
+				set whereClause = concat(whereClause, " and pi.total_amount < ", amount);
+				END IF;
+			END IF;
+			
+		IF( ledgerAccountId > 0) THEN
+			set whereClause = concat(whereClause, " and pi.invoice_master_id in (SELECT coa.invoice_master_id FROM supplier_coa_tx_invoice coa WHERE coa.ledger_account_id = ", ledgerAccountId,")");
+		END IF;	
+	SET @stmt= 'select
+										
+					pi.invoice_master_id,
+					pi.rid,
+					pi.invoice_number,
+					pi.supplier_id,
+					pi.invoice_date,
+					pi.invoice_due_date,
+					pi.purchase_order_id,
+					pi.sub_total,
+					pi.total_amount,
+					pi.status,
+					pi.status_color_for_ui_cell,
+					pi.po_master_id,
+					pi.date_created,
+					pi.updated_by_user_id,
+					pi.created_by_user_id,
+					pi.date_updated,
+					pi.payment_term_id,
+					pi.reference_number,
+					pi.foreign_currency_amount,
+					pi.foreign_currency,
+					pi.exchange_rate,
+					pi.supplier_invoice_number,
+					pi.supplier_po_date,
+					pi.supplier_po_number,
+					pi.is_multi_currency,
+					pi.is_cash_invoice,
+					pi.place_of_supply,
+					pi.supplier_name,
+					pi.payment_term_name,
+					pi.relationship_name,
+					pi.payment_term_days,
+					pi.project_number,
+					pi.project_name,
+					pi.foreign_currency_icon,
+					pi.is_xero_uploaded,
+					pi.is_approval_required,
+					pi.approved_by_emp_id,
+					pi.reviewed_by_emp_id,
+					pi.created_by_emp_id,
+					pi.is_approved,
+					pi.is_rejected,
+					pi.approver_emp_id,
+					pi.currency_code,
+					pi.is_po_conversion,
+					pi.is_id_conversion,
+					pi.customer_inquiry_number,
+					pi.customer_rfq_number,
+					pi.customer_po_number,
+					pi.supplier_quote_number,
+					pi.user_status,
+					pi.inquiry_master_id,
+					pi.inquiry_number,
+					pi.sales_quotation_master_id,
+					pi.quotation_number,
+					pi.sales_order_master_id,
+					pi.so_number,
+					sum(p.payment_amount) as totalPaymentMade,
+                    count(p.supplier_payment_id) as paymentCount,
+                    sum(a.amount_applied) as creditApplied
+				from supplier_invoice_master pi
+                left join supplier_payments p on pi.invoice_master_id = p.invoice_master_id
+                left join supplier_debit_memo_applied a on pi.invoice_master_id = a.invoice_master_id
+				where';
+		SET @stmt1=CONCAT(@stmt,' ', whereClause, ' group by pi.invoice_master_id order by pi.invoice_master_id desc LIMIT ', pageSize, ' OFFSET ', pageNumber*pageSize);
+		PREPARE stmt2 FROM @stmt1;
+		EXECUTE stmt2;
+		DEALLOCATE PREPARE stmt2;
+	END$$
+DELIMITER ;
+
+ALTER TABLE `alpide-purchase`.`supplier_po_master` ADD COLUMN total_po_multicurrency_amount double  DEFAULT 0.0;
+ALTER TABLE `alpide-purchase`.`supplier_invoice_master` ADD COLUMN invoice_multicurrency_total_amount double  DEFAULT 0.0;
+ALTER TABLE `alpide-purchase`.`supplier_debit_memo_details` ADD COLUMN item_variant_stock_id bigint  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_sales_order_details` ADD COLUMN stock_deduction_type varchar(120)  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_invoice_details` ADD COLUMN stock_deduction_type varchar(120)  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_so_shipment_details` ADD COLUMN stock_deduction_type varchar(120)  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_so_package_details` ADD COLUMN stock_deduction_type varchar(120)  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_sales_order_additional_info` ADD COLUMN country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_sales_order_additional_info` ADD COLUMN relationship_country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_invoice_additional_info` ADD COLUMN country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_invoice_additional_info` ADD COLUMN relationship_country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_so_package_additional_info` ADD COLUMN country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_so_package_additional_info` ADD COLUMN relationship_country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_so_package_additional_info` ADD COLUMN relationship_phone_number varchar(100)  DEFAULT null;
+ALTER TABLE `alpide-sales`.`customer_so_shipment_additional_info` ADD COLUMN country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_so_shipment_additional_info` ADD COLUMN relationship_country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_so_shipment_additional_info` ADD COLUMN relationship_phone_number varchar(100)  DEFAULT null;
+
+ALTER TABLE `alpide-sales`.`customer_sales_quotation_master` ADD COLUMN multicurrency_total_amount double  DEFAULT 0.0;
+ALTER TABLE `alpide-sales`.`customer_sales_order_master` ADD COLUMN multicurrency_total_amount double  DEFAULT 0.0;
+ALTER TABLE `alpide-sales`.`customer_invoice_master` ADD COLUMN multicurrency_total_amount double  DEFAULT 0.0;
+ALTER TABLE `alpide-sales`.`customer_sales_quotation_additional_info` ADD COLUMN country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_sales_quotation_additional_info` ADD COLUMN relationship_country_calling_code int  DEFAULT 0;
+ALTER TABLE `alpide-sales`.`customer_credit_memo_details` ADD COLUMN item_variant_stock_id bigint  DEFAULT null;
+ALTER TABLE `alpide-purchase`.`supplier_debit_memo_master` ADD COLUMN is_multicurrency int  DEFAULT 0
+
+
+
+
+
+
+
+
+
+
+
+
+
