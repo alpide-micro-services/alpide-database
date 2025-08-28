0db40045413540863ac97c5c1cacd0e279b7af21
Chirag Tyagi <chiragtyagi@Chirags-MacBook-Pro.local>
2025-08-28 12:18:04 +0530

    query for adding a column in the blog

--- release/Ver-1/06-columnaddQuery.sql
+++ release/Ver-1/06-columnaddQuery.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `alpide-inventory`.`inventory_item` ADD COLUMN customer_sku VARCHAR(200) DEFAULT NULL;
+ALTER TABLE `alpide-commerce`.`ecom_blog_master` ADD COLUMN alt_tag VARCHAR(250) DEFAULT NULL;
