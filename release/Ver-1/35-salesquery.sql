a0e8507f3f1bf96a841f0d5ff9d25faff49dd3fe
Alpide Tech. <alpidetech.@Alpides-MacBook-Pro.local>
2026-05-05 16:50:10 +0530

    queries for quation

---
+++ release/Ver-1/35-salesquery.sql
@@ -0,0 +1,13 @@
+
+ALTER TABLE `alpide-crm`.`crm_account` 
+DROP INDEX `account_code_UNIQUE` ;
+;
+ALTER TABLE `alpide-crm`.`crm_opportunity_v2` 
+DROP INDEX `opportunity_code` ;
+;
+ALTER TABLE `alpide-sales`.`customer_sales_quotation_master`
+  ADD COLUMN `opportunity_id`   BIGINT       NULL DEFAULT NULL,
+  ADD COLUMN `opportunity_name` VARCHAR(255) NULL DEFAULT NULL,
+  ADD COLUMN `crm_lead_id`      BIGINT       NULL DEFAULT NULL,
+  ADD COLUMN `lead_name`        VARCHAR(255) NULL DEFAULT NULL,
+  ADD COLUMN `crm_account_id`   BIGINT       NULL DEFAULT NULL;
