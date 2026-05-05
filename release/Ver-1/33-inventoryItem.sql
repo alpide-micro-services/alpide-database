2624d92f83440aa144b886daf461373a71a9eec7
Alpide Tech. <alpidetech.@Alpides-MacBook-Pro.local>
2026-05-05 16:48:42 +0530

    queries for quation

--- release/Ver-1/33-inventoryItem.sql
+++ release/Ver-1/33-inventoryItem.sql
@@ -1,2 +1,15 @@
 ALTER TABLE `alpide-inventory`.`inventory_item` 
 ADD COLUMN `is_active` INT NULL DEFAULT 1 AFTER `is_service_product`;
+
+ALTER TABLE `alpide-crm`.`crm_account` 
+DROP INDEX `account_code_UNIQUE` ;
+;
+ALTER TABLE `alpide-crm`.`crm_opportunity_v2` 
+DROP INDEX `opportunity_code` ;
+;
+ALTER TABLE `alpide-sales`.`customer_sales_quotation_master`
+  ADD COLUMN `opportunity_id`   BIGINT       NULL DEFAULT NULL,
+  ADD COLUMN `opportunity_name` VARCHAR(255) NULL DEFAULT NULL,
+  ADD COLUMN `crm_lead_id`      BIGINT       NULL DEFAULT NULL,
+  ADD COLUMN `lead_name`        VARCHAR(255) NULL DEFAULT NULL,
+  ADD COLUMN `crm_account_id`   BIGINT       NULL DEFAULT NULL;
